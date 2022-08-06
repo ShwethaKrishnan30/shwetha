b8eacbd13b15bb8b006fb7d3278c15210b864f4e
ShwethaKrishnan30 <110732153+ShwethaKrishnan30@users.noreply.github.com>
2022-08-06 22:58:56 +0530

    Assignment Day 1

---
+++ NegativeToPositive.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class NegativeToPositive {
+
+	public static void main(String[] args) {
+		int n= -13;
+				if (n<0) {
+					System.out.println("The given value is positive");
+				}
+				else {
+					System.out.println("The given value is negative");
+				}
+		}
+	}
