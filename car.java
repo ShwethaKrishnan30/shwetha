97ad200778c10f202c32d7ca4f8d9a4a2bcb9075
ShwethaKrishnan30 <110732153+ShwethaKrishnan30@users.noreply.github.com>
2022-08-06 20:11:21 +0530

    Add files via upload

---
+++ car.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class car {
+	
+	public car() {
+		// TODO Auto-generated constructor stub
+	}
+  byte series = -19;
+  short modelNumber = -8767;
+  int seriesnumber = -234;
+  long detailedprice = -86778888811111134L;
+  float wheeltype = -9876.234F;
+  double detailwheeltype= -1.23456;
+  boolean istankfull = false;
+  char seriesname = 'a';
+  String car = "maruti";
+ }
+
