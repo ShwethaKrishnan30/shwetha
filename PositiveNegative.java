fa8f4f520aa39c21fac5f059667f2f89eb44ddfa
ShwethaKrishnan30 <110732153+ShwethaKrishnan30@users.noreply.github.com>
2022-08-06 21:28:52 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class PositiveNegative {
+	public static void main(String[] args) {
+	int n= 13;
+			if (n>0) {
+				System.out.println("The given value is positive");
+			}
+			else {
+				System.out.println("The given value is negative");
+			}
+	}
+}
